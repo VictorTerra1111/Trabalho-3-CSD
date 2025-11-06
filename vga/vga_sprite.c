856cc66c9db8f114cecc3a869c3e57c10f7ab4b5
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-11-06 17:43:33 -0300

    Add files via upload

--- vga/vga_sprite.c
+++ vga/vga_sprite.c
@@ -94,10 +94,14 @@ void move_object(struct object_s *obj)
 	if (--obj->speedxcnt == 0) {
 		obj->speedxcnt = obj->speedx;
 		obj->posx = obj->posx + obj->dx;
+
+		if (obj->posx + obj->spriteszx >= VGA_WIDTH || obj->posx <= 0) obj->dx = -obj->dx; 
 	}
 	if (--obj->speedycnt == 0) {
 		obj->speedycnt = obj->speedy;
 		obj->posy = obj->posy + obj->dy;
+
+		if (obj->posy + obj->spriteszy >= VGA_HEIGHT || obj->posy <= 0) obj->dy = -obj->dy;
 	}
 
 	if ((obj->speedx == obj->speedxcnt) || (obj->speedy == obj->speedycnt)) {
@@ -140,29 +144,54 @@ int get_input()
 	return keys;
 }
 
+int detect_collision(struct object_s *obj1, struct object_s *obj2)
+{
+	if (obj1->posx < obj2->posx + obj2->spriteszx &&
+		obj1->posx + obj1->spriteszx > obj2->posx &&
+		obj1->posy < obj2->posy + obj2->spriteszy &&
+		obj1->posy + obj1->spriteszy > obj2->posy) return 1;
+
+	return 0;
+}
+
 /* main game loop */
 int main(void)
 {
-	struct object_s enemy1, enemy2;
+	struct object_s enemy1, enemy2, enemy3;
 
 	init_display();
 	init_input();
 
 	init_object(&enemy1, monster1a[0], monster1b[0], 0, 11, 8, 30, 35, 1, 1, 3, 3);
 	init_object(&enemy2, monster1a[0], monster1b[0], 0, 11, 8, 170, 160, -1, 0, 1, 1);
+	init_object(&enemy3, monster1a[0], monster1b[0], 0, 11, 8, 15, 80, -1, 1, 5, 3);
+
+	display_print("hey", 0, 180, 1, CYAN);
 
 	while (1) {
 		move_object(&enemy1);
 		move_object(&enemy2);
+		move_object(&enemy3);
+
+		if (detect_collision(&enemy1, &enemy2)) display_print("collision: 1 and 2", 0, 180, 1, CYAN);
+		if (detect_collision(&enemy1, &enemy3)) display_print("collision: 1 and 3", 0, 180, 1, CYAN);
+		if (detect_collision(&enemy2, &enemy3)) display_print("collision: 2 and 3", 0, 180, 1, CYAN);
 		
 		// you can change the direction, speed, etc...
-		
+				
 		// you can get input keys
 		if (get_input() == KEY_CENTER) {
+			enemy2.dx = 0;
+		}
+		if (get_input() == KEY_LEFT) {
+			enemy2.dx = -1;
+		}
+		if (get_input() == KEY_RIGHT) {
+			enemy2.dx = 1;
 		}
 		
 		// game loop frame limiter
-		delay_ms(200);
+		delay_ms(50);
 	}
 
 	return 0;
