8051e987d286edd6173cfe5a03cf245ad1b76ac5
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-11-15 12:10:20 -0300

    Creates .h
    Removed unnecessary comments and cleaned up the header file.

---
+++ game_sprites.h
@@ -0,0 +1,24 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+
+#include <hf-risc.h>
+#include "vga_drv.h"
+
+extern char monster1_an1[8][8];
+extern char monster1_an2[8][8];
+
+extern char monster2_an1[8][8];
+extern char monster2_an2[8][8];
+
+extern char monster3_an1[8][8];
+extern char monster3_an2[8][8];
+
+extern char explosion[8][8];
+
+extern char player[8][8];
+
+extern char big_guy[10][30];
+
+extern char shield[10][20];
+
+#endif
