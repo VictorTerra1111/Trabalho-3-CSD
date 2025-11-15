2e97fbcd94e52874ed537efe2d82b6970e1261e6
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-11-15 12:18:23 -0300

    Created header file for functions of the game

---
+++ vga/game.h
@@ -0,0 +1,44 @@
+#ifndef GAME_ENGINE_H
+#define GAME_ENGINE_H
+
+#include <hf-risc.h>
+#include "vga_drv.h"
+#include "game_sprites.h"
+
+void draw_sprite(unsigned int x, unsigned int y, char *sprite,
+                 unsigned int sizex, unsigned int sizey, int color);
+
+struct object_s {
+    char *sprite_frame[3];
+    char spriteszx, spriteszy, sprites;
+    int cursprite;
+    unsigned int posx, posy;
+    int dx, dy;
+    int speedx, speedy;
+    int speedxcnt, speedycnt;
+};
+
+void init_object(struct object_s *obj, char *spritea, char *spriteb,
+                 char *spritec, char spriteszx, char spriteszy,
+                 int posx, int posy, int dx, int dy, int spx, int spy);
+
+void draw_object(struct object_s *obj, char chgsprite, int color);
+
+void move_object(struct object_s *obj);
+
+void init_display(void);
+
+enum {
+    KEY_CENTER = 0x01,
+    KEY_UP     = 0x02,
+    KEY_LEFT   = 0x04,
+    KEY_RIGHT  = 0x08,
+    KEY_DOWN   = 0x10
+};
+
+void init_input(void);
+int get_input(void);
+
+int detect_collision(struct object_s *obj1, struct object_s *obj2);
+
+#endif
