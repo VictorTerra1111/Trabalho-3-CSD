1ef00c6ec62a19cdcecf3d7db2c56e84c3d6b16c
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-11-20 22:14:31 -0300

    Change dimensions of sprite arrays in game_sprites.h
    Updated dimensions of monster animation arrays and added bullet array.

--- SpaceInVaders/vitu/game_sprites.h
+++ SpaceInVaders/vitu/game_sprites.h
@@ -1,8 +1,8 @@
 #ifndef SPRITES_H
 #define SPRITES_H
 
-extern char monster1_an1[8][11];
-extern char monster1_an2[8][11];
+extern char monster1_an1[8][8];
+extern char monster1_an2[8][8];
 
 extern char monster2_an1[8][11];
 extern char monster2_an2[8][11];
@@ -10,12 +10,12 @@ extern char monster2_an2[8][11];
 extern char monster3_an1[8][11];
 extern char monster3_an2[8][11];
 
-extern char explosion[8][8];
-
 extern char player[8][11];
 
 extern char mothership[10][30];
 
 extern char shield[10][20];
+
+extern char bullet[2][1];
 
 #endif
