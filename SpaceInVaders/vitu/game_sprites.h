490d8d5ae40c37ffdf57efcd0cea5d270acf4529
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-11-20 21:52:09 -0300

    Increase dimensions of sprite arrays
    Updated sprite arrays to accommodate larger dimensions.

--- SpaceInVaders/vitu/game_sprites.h
+++ SpaceInVaders/vitu/game_sprites.h
@@ -1,33 +1,21 @@
 #ifndef SPRITES_H
 #define SPRITES_H
 
-extern char monster1_an1[8][8];
-extern char monster1_an2[8][8];
+extern char monster1_an1[8][11];
+extern char monster1_an2[8][11];
 
-extern char monster2_an1[8][8];
-extern char monster2_an2[8][8];
+extern char monster2_an1[8][11];
+extern char monster2_an2[8][11];
 
-extern char monster3_an1[8][8];
-extern char monster3_an2[8][8];
+extern char monster3_an1[8][11];
+extern char monster3_an2[8][11];
 
 extern char explosion[8][8];
 
-extern char player[8][8];
+extern char player[8][11];
 
-extern char spaceship[8][11];
-
-extern char alien2a[8][11];
-extern char alien2b[8][11];
-
-extern char big_guy[10][30];
+extern char mothership[10][30];
 
 extern char shield[10][20];
-
-extern char alien_medium_a[8][11];
-extern char alien_medium_b[8][11];
-
-extern char alien_strong_a[8][11];
-extern char alien_strong_b[8][11];
-
 
 #endif
